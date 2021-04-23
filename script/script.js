194c8a3f51f5a647e1deeb1889afb9439ffbc19d
uroge <67282212+uroge@users.noreply.github.com>
2021-04-23 08:43:26 +0200

    Conditionaly checked the width of a row in order to change number of images per page and max pages allowed on smaller screens

--- script/script.js
+++ script/script.js
@@ -2,12 +2,11 @@
 var readMoreButtons = document.querySelectorAll('.read-more-btn');
 var paragraphs = document.querySelectorAll('.middle__paragraph');
 
-
 readMoreButtons.forEach(function(btn) {
     btn.addEventListener('click', function(e) {
         e.preventDefault();
-        if(paragraphs[parseInt(btn.id)].style.height !== '12rem') {
-            paragraphs[parseInt(btn.id)].style.height = '12rem'
+        if(paragraphs[parseInt(btn.id)].style.height !== '14rem') {
+            paragraphs[parseInt(btn.id)].style.height = '14rem';
             btn.innerHTML = '<i class="fas fa-angle-up"></i> Read less';
         } else {
             paragraphs[parseInt(btn.id)].style.height = '6.5rem';
@@ -20,16 +19,32 @@ readMoreButtons.forEach(function(btn) {
 var sliderArrowLeft = document.getElementById('arrow-left');
 var sliderArrowRight = document.getElementById('arrow-right');
 
-var width = document.querySelector('.latest-works__row-2').offsetWidth;
-window.addEventListener("resize", function () {
-    width = document.querySelector('.latest-works__row-2').offsetWidth;
-});
+var imagesPerPage = 4;
 
 var imagesContainer = document.querySelector('.latest-works__images');
-
 var numberOfImages = document.querySelectorAll('.latest-works__image').length;
 
-var maxPages = Math.floor(numberOfImages / 4);
+var maxPages = Math.floor(numberOfImages / imagesPerPage);
+
+var width = document.querySelector('.latest-works__row-2').offsetWidth;
+window.addEventListener('resize', function () {
+    width = document.querySelector('.latest-works__row-2').offsetWidth;
+    console.log(width);
+
+    if(width < 480) {
+        imagesPerPage = 1;
+        maxPages = Math.floor(numberOfImages / imagesPerPage) - 1;
+    } else if(width < 784) {
+        imagesPerPage = 2;
+        maxPages = Math.floor(numberOfImages / imagesPerPage) - 1;
+    } else if (width < 960) {
+        imagesPerPage = 3;
+        maxPages = Math.floor(numberOfImages / imagesPerPage);
+    } else {
+        imagesPerPage = 4;
+        maxPages = Math.floor(numberOfImages / imagesPerPage);
+    }
+});
 
 var index = 0;
 
@@ -53,7 +68,7 @@ sliderArrowRight.addEventListener('click', function() {
         index = index + 1;
         sliderArrowLeft.classList.remove('disabled');
         imagesContainer.style.transform = 'translateX(' + index * -width + 'px)';
-
+        
         if(index === maxPages) {
             sliderArrowRight.classList.add('disabled');
         }
