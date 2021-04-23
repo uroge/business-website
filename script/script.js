a612d25f531baf4092d41fc216a0ab070f47db12
uroge <67282212+uroge@users.noreply.github.com>
2021-04-23 13:04:25 +0200

    Refactored javscript code

--- script/script.js
+++ script/script.js
@@ -1,78 +1,83 @@
-// READ MORE BUTTON
-var readMoreButtons = document.querySelectorAll('.read-more-btn');
-var paragraphs = document.querySelectorAll('.middle__paragraph');
+(function(){
+    var paragraphs = document.querySelectorAll('.middle__paragraph'),
+        sliderArrowLeft = document.getElementById('arrow-left'),
+        sliderArrowRight = document.getElementById('arrow-right'),
+        imagesPerSlide = 4,
+        imagesContainer = document.querySelector('.latest-works__images'),
+        images = document.querySelectorAll('.latest-works__image'),
+        numberOfImages = images[0] ? images.length - 1 : null,
+        maxSlides = Math.floor(numberOfImages / imagesPerSlide),
+        imagesRow = document.querySelector('.latest-works__row-2'),
+        sliderWidth = imagesRow ? imagesRow.offsetWidth : null,
+        slideIndex = 0;
 
-readMoreButtons.forEach(function(btn) {
-    btn.addEventListener('click', function(e) {
+    document.body.addEventListener('click', function(e) {
         e.preventDefault();
-        if(paragraphs[parseInt(btn.id)].style.height !== '14rem') {
-            paragraphs[parseInt(btn.id)].style.height = '14rem';
-            btn.innerHTML = '<i class="fas fa-angle-up"></i> Read less';
-        } else {
-            paragraphs[parseInt(btn.id)].style.height = '6.5rem';
-            btn.innerHTML = '<i class="fas fa-angle-right"></i> Read more';
+        var target = e.target;
+        if (target.classList.contains('js-read-more-btn')) {
+            if(paragraphs) {
+                if(!paragraphs[parseInt(target.getAttribute('data-id'))].classList.contains('middle__paragraph-active')) {
+                    paragraphs[parseInt(target.getAttribute('data-id'))].classList.add('middle__paragraph-active');
+                    target.innerHTML = '<i class="fas fa-angle-up"></i> Read less';
+                } else {
+                    paragraphs[parseInt(target.getAttribute('data-id'))].classList.remove('middle__paragraph-active');
+                    target.innerHTML = '<i class="fas fa-angle-right"></i> Read more';
+                }
+            }
         }
     });
-});
 
-// SLIDER
-var sliderArrowLeft = document.getElementById('arrow-left');
-var sliderArrowRight = document.getElementById('arrow-right');
-
-var imagesPerPage = 4;
-
-var imagesContainer = document.querySelector('.latest-works__images');
-var numberOfImages = document.querySelectorAll('.latest-works__image').length;
-
-var maxPages = Math.floor(numberOfImages / imagesPerPage);
-
-var width = document.querySelector('.latest-works__row-2').offsetWidth;
-window.addEventListener('resize', function () {
-    width = document.querySelector('.latest-works__row-2').offsetWidth;
-    console.log(width);
-
-    if(width < 480) {
-        imagesPerPage = 1;
-        maxPages = Math.floor(numberOfImages / imagesPerPage) - 1;
-    } else if(width < 784) {
-        imagesPerPage = 2;
-        maxPages = Math.floor(numberOfImages / imagesPerPage) - 1;
-    } else if (width < 960) {
-        imagesPerPage = 3;
-        maxPages = Math.floor(numberOfImages / imagesPerPage);
-    } else {
-        imagesPerPage = 4;
-        maxPages = Math.floor(numberOfImages / imagesPerPage);
-    }
-});
-
-var index = 0;
-
-sliderArrowLeft.addEventListener('click', function() {
-    if(!(sliderArrowLeft.classList.value.split(' ').includes('disabled'))){
-        index = index - 1;
-
-        if(index === 0) {
-            sliderArrowLeft.classList.add('disabled');
-        }
+    window.addEventListener('resize', function () {
+        sliderWidth = imagesRow.offsetWidth;
         
-        imagesContainer.style.transform = 'translateX(' + index * -width +'px)';
-        sliderArrowRight.classList.remove('disabled');
-    } else {
-        return;
-    }
-});
-
-sliderArrowRight.addEventListener('click', function() {
-    if(!(sliderArrowRight.classList.value.split(' ').includes('disabled'))) {
-        index = index + 1;
-        sliderArrowLeft.classList.remove('disabled');
-        imagesContainer.style.transform = 'translateX(' + index * -width + 'px)';
-        
-        if(index === maxPages) {
-            sliderArrowRight.classList.add('disabled');
+        if(sliderWidth) {
+            if(sliderWidth < 480) {
+                imagesPerSlide = 1;
+                maxSlides = Math.floor(numberOfImages / imagesPerSlide);
+            } else if(sliderWidth < 784) {
+                imagesPerSlide = 2;
+                maxSlides = Math.floor(numberOfImages / imagesPerSlide);
+            } else if(sliderWidth < 960) {
+                imagesPerSlide = 3;
+                maxSlides = Math.floor(numberOfImages / imagesPerSlide);
+            } else {
+                imagesPerSlide = 4;
+                maxSlides = Math.floor(numberOfImages / imagesPerSlide);
+            }
         }
-    }else {
-        return;
+    });
+
+
+    if(sliderArrowLeft) {
+        sliderArrowLeft.addEventListener('click', function() {
+            if(!(sliderArrowLeft.classList.contains('disabled'))){
+                slideIndex = slideIndex - 1;
+    
+                if(slideIndex === 0) {
+                    sliderArrowLeft.classList.add('disabled');
+                }
+                
+                imagesContainer.style.transform = 'translateX(' + slideIndex * -sliderWidth +'px)';
+                sliderArrowRight.classList.remove('disabled');
+            } else {
+                return;
+            }
+        });
     }
-});
+    
+    if(sliderArrowRight) {
+        sliderArrowRight.addEventListener('click', function() {
+            if(!(sliderArrowRight.classList.contains('disabled'))) {
+                slideIndex = slideIndex + 1;
+                sliderArrowLeft.classList.remove('disabled');
+                imagesContainer.style.transform = 'translateX(' + slideIndex * -sliderWidth + 'px)';
+    
+                if(slideIndex === maxSlides) {
+                    sliderArrowRight.classList.add('disabled');
+                }
+            } else {
+                return;
+            }
+        });
+    }
+})();
